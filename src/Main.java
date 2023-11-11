281fedbae56f9c6b98a974872e462dd91b8c421b
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-11-11 16:32:59 +0300

    Add second task

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String theLongest = null;
-        int theMaxLength = 0;
-        String theShortest = null;
-        int theMinLength = 0;
 
         String[] strs = new String[3];
         for (int i = 0; i < strs.length; i++) {
             strs[i] = scanner.nextLine();
         }
 
+        String theLongest = null;
+        int theMaxLength = 0;
         for (int i = 0; i < strs.length; i++) {
             if (strs[i].length() > theMaxLength) {
                 theLongest = strs[i];
@@ -20,7 +19,8 @@ public class Main {
             }
         }
 
-        theMinLength = strs[0].length();
+        int theMinLength = strs[0].length();
+        String theShortest = strs[0];
         for (int i = 0; i < strs.length; i++) {
             if (strs[i].length() < theMinLength) {
                 theShortest = strs[i];
@@ -30,5 +30,25 @@ public class Main {
 
         System.out.println("The longest string is " + "'" + theLongest + "'" + " with length " + theMaxLength);
         System.out.println("The shortest string is " + "'" + theShortest + "'" + " with length " + theMinLength);
+
+        int[] sizeOfString = new int[strs.length];
+        int[] count= new int[strs.length];
+        for (int i = 0; i < strs.length; i++) {
+            sizeOfString[i] = strs[i].length();
+            count[i] = i;
+        }
+        for (int i =0;i< strs.length-1;i++){
+            for (int j = i+1;j< strs.length;j++){
+                if (sizeOfString[i]>sizeOfString[j]){
+                    int temp = count[i];
+                    count[i] = count[j];
+                    count[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(count));
+        for (int i = 0; i < strs.length; i++) {
+            System.out.println(strs[count[i]]);
+        }
     }
 }
