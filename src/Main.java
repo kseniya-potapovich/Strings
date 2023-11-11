89bdfe16ce9a056857c51170c1d32456d69a10c8
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-11-11 16:58:59 +0300

    Add third task

--- src/Main.java
+++ src/Main.java
@@ -31,24 +31,37 @@ public class Main {
         System.out.println("The longest string is " + "'" + theLongest + "'" + " with length " + theMaxLength);
         System.out.println("The shortest string is " + "'" + theShortest + "'" + " with length " + theMinLength);
 
+        System.out.println("-------------------");
         int[] sizeOfString = new int[strs.length];
-        int[] count= new int[strs.length];
+        int[] count = new int[strs.length];
         for (int i = 0; i < strs.length; i++) {
             sizeOfString[i] = strs[i].length();
             count[i] = i;
         }
-        for (int i =0;i< strs.length-1;i++){
-            for (int j = i+1;j< strs.length;j++){
-                if (sizeOfString[i]>sizeOfString[j]){
+        for (int i = 0; i < strs.length - 1; i++) {
+            for (int j = i + 1; j < strs.length; j++) {
+                if (sizeOfString[i] > sizeOfString[j]) {
                     int temp = count[i];
                     count[i] = count[j];
                     count[j] = temp;
                 }
             }
         }
-        System.out.println(Arrays.toString(count));
         for (int i = 0; i < strs.length; i++) {
             System.out.println(strs[count[i]]);
+        }
+
+        System.out.println("-------------------");
+        double sum = 0;
+        for (int i = 0; i < strs.length; i++) {
+            sum += sizeOfString[i];
+        }
+        sum = sum / strs.length;
+        //System.out.println(sum);
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].length() < sum) {
+                System.out.println(strs[i] + "\t" + strs[i].length());
+            }
         }
     }
 }
