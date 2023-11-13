6a2104aab0be3ebd66c81e76a5cc115da7af80cd
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-11-13 12:46:29 +0300

    Add commit

--- src/Main.java
+++ src/Main.java
@@ -63,5 +63,25 @@ public class Main {
                 System.out.println(strs[i] + "\t" + strs[i].length());
             }
         }
+
+        System.out.println("-------------------");
+        String[] words1 = strs[0].split(" ");
+        String[] words2 = strs[1].split(" ");
+        String[] words3 = strs[2].split(" ");
+        int size = words1.length + words2.length + words3.length;
+        /*for (int i =0;i<words1.length;i++){
+            System.out.println(words1[i]);
+        }
+        for (int i =0;i< words2.length;i++){
+            System.out.println(words2[i]);
+        }
+        for (int i =0;i< words3.length;i++){
+            System.out.println(words3[i]);
+        }*/
+        System.out.println("Пункта 4 не будет");
+
+        System.out.println("-------------------");
+        String text = scanner.nextLine();
+        System.out.println("Пункта 5 тоже кажись не будет");
     }
 }
